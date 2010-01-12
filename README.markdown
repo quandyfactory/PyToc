9326b4d00fafb511f36575d5f875605c92857d0e
Ryan McGreal <ryan@quandyfactory.com>
2010-01-11 21:53:21 -0500

    v. 0.2 added format_contents(), fixed multiple small bugs

--- README.markdown
+++ README.markdown
@@ -17,12 +17,23 @@ Released under the GNU General Public Licence, Version 2:
 
 ## This Version
 
-* Version: 0.11
+* Version: 0.2
 
 * Release Date: 2010-01-11
 
 ## Revision History
 
+### Version: 0.2
+
+* Release Date: 2010-01-11
+
+* Changes:
+
+    * Fixed bugs
+    * Cleaned up code
+    * Added format_contents() to generate an HTML table of contents
+    * Added count_chars() for use inside format_contents()
+    
 ### Version: 0.11
 
 * Release Date: 2010-01-11
@@ -31,7 +42,7 @@ Released under the GNU General Public Licence, Version 2:
 
     * Fixed formatting
     * Added heading permalink
-    
+        
 ### Version: 0.1
 
 * Release Date: 2010-01-11
@@ -40,3 +51,41 @@ Released under the GNU General Public Licence, Version 2:
 
     * First Commit
     
+###Usage
+
+It's pretty simple to use. Download pytoc.py and save it somewhere in your PATH. Here's a sample usage.
+
+    import pytoc
+    import urllib
+    url = 'http://quandyfactory.com/projects/40/pytoc'
+    page = urllib.urlopen(url)
+    html_in = page.read()
+    toc_list, html_out = pytoc.make_toc(html_in, [2, 3, 4])
+
+####Using make_toc()
+
+The function `pytoc.make_toc()` takes two parameters:
+
+1. `html_in` - a string of the HTML on which to generate the table of contents; and
+2. `levels` - a list of integers corresponding to the heading levels to be included in the table of contents, e.g. 3 corresponds to `<h3>`. The default value is `[3, 4]`.
+
+This function generates two return values:
+
+1. `toc_list` - a list of tuples containing `(section_number, heading text)` for the table of contents; and
+2. `html_out` - the html string, modified to include anchors so the table of contents can link to the sections. 
+
+####Using format_contents()
+
+You can take the list returned by `toc_list` and roll your own HTML table of contents; or you can use the built-in generator, `pytoc.format_contents()`.
+
+This function takes 3 parameters:
+
+1. `toc_list` - this is the list output by `make_toc()`. 
+2. `levels` - again, a list of integers corresponding to the heading levels to be included in the table of contents, e.g. 3 corresponds to `<h3>`. The default value is `[3, 4]`.
+3. `id` - this is an HTML id attribute, used when generating the table id and td classes. The default value is `toc`.
+
+It returns one value:
+
+1. `html_out` - the HTML table of contents. Yes, it's an HTML `<table>`.
+
+That's pretty much it. You can use the toc_list to roll your own presentation for your table of contents.
--- pytoc.py
+++ pytoc.py
@@ -4,7 +4,7 @@
 This code takes an HTML page and generates a table of contents.
 """
 
-__version__ = '0.11'
+__version__ = '0.2'
 __releasedate__ = '2010-01-11'
 __author__ = 'Ryan McGreal <ryan@quandyfactory.com>'
 __homepage__ = 'http://quandyfactory.com/projects/40/pytoc'
@@ -15,7 +15,7 @@ __copyright__ = 'Copyright (C) 2010 by Ryan McGreal. Licenced under GPL version 
 from BeautifulSoup import BeautifulSoup as bs
 import re
  
-def make_toc(html, levels=[3, 4]):
+def make_toc(html_in, levels=[3, 4]):
     """
     Takes an HTML page and generates a table of contents.
     headings takes a list of HTML heading levels from 1 to 6,
@@ -26,13 +26,13 @@ def make_toc(html, levels=[3, 4]):
     numbering = [[level, 0] for level in levels]
  
     # initialize toc list
-    toc = []
+    toc_list = []
  
     # take a beautifulsoup parse tree of the html
-    soup = bs(html)
+    soup = bs(html_in)
  
     # generate a regex to find headings for the table of contents
-    heading_regex = '|'.join([str(l) for l in levels])
+    heading_regex = '|'.join([str(level) for level in levels])
     search = re.compile('^h(%s)$' % (heading_regex))
  
     # capture a list of headings we care about
@@ -44,19 +44,57 @@ def make_toc(html, levels=[3, 4]):
         for number in numbering:
             if number[0] == this_num:
                 numbering[numbering.index(number)][1] += 1
-            elif number[0] > this_num:
+            if number[0] > this_num:
                 numbering[numbering.index(number)][1] = 0
  
         # generate toc_anchor
         this_toc_anchor = '.'.join(['%s' % number[1] for number in numbering])
- 
+        this_toc_anchor = this_toc_anchor.replace('.0', '')
+
         # add to contents
-        toc.append((this_toc_anchor, heading.text))
+        toc_list.append((this_toc_anchor, heading.string))
  
         # update heading with anchor
-        heading.find(text=heading.text).replaceWith('<a name="toc_%s">%s %s</a>  <a class="heading_permalink" title="Link to this section" href="#toc_%s">&para;<a>' % (
-            this_toc_anchor, this_toc_anchor, heading.text, this_toc_anchor)
-            )
+        heading.find(text=heading.string).replaceWith('<a href="#toc_%s" title="Link directly to section %s">%s</a><a name="toc_%s"></a> <span title="Section %s: %s">%s</span> <a href="#" title="Back to top"> &uarr; </a>' % (this_toc_anchor, this_toc_anchor, this_toc_anchor, this_toc_anchor, this_toc_anchor, heading.string, heading.string))
  
-    return toc, str(soup)
+    html_out = str(soup)
+    return toc_list, html_out
  
+ 
+def count_chars(string, char):
+    """
+    Returns an integer of how many times char appears in string
+    """
+    charcount = 0
+    for s in string:
+        if s == char:
+            charcount += 1
+    return charcount
+
+
+def format_contents(toc_list, levels=[3, 4], id="toc"):
+    """
+    Takes a toc_list from make_toc() and returns an HTML table of contents
+    """
+    html = []
+    addline = html.append
+    addline('<h2><a name="%s">Contents</a></h2>' % id)
+    addline('<table id="%s_table">' % id)
+    for t in toc_list:
+        addline('<tr>')
+        dots = count_chars(t[0], '.')
+        if dots == 0:
+            addline('<td class="%s_section_number">%s</td><td colspan="%s"><a href="#%s_%s">%s</a></td>' % (
+                id, t[0], len(levels), id, t[0], t[1])
+                )
+        else:
+            for loop in range(dots):
+                addline('<td class="%s_section_number"></a>' % (id))
+            addline('<td class="%s_section_number">%s</a>' % (id, t[0]))
+            addline('<td colspan="%s"><a href="#%s_%s">%s</a></td>' % (len(levels)-dots, id, t[0], t[1]))
+        addline('</tr>')
+    addline('</table>')
+    html_out = '\n'.join(html)
+    return html_out
+    
+    
